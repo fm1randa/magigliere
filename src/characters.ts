b9b71a52ba83790327c81461e7df1e90ea53dfdc
Filipe Miranda <email@filipemiranda.dev>
2025-02-24 23:59:27 -0300

    refactor: :label: update character interface

--- src/characters.ts
+++ src/characters.ts
@@ -4,9 +4,32 @@ const URL = "https://hp-api.onrender.com/api";
 
 const route = (path: TemplateStringsArray) => `${URL}${path}`;
 
+interface Wand {
+  wood: string;
+  core: string;
+  length: number;
+}
+
 interface Character {
+  id: string;
   name: string;
+  alternate_names: string[];
+  species: string;
+  gender: string;
   house: string;
+  dateOfBirth: string;
+  yearOfBirth: number;
+  wizard: boolean;
+  ancestry: string;
+  eyeColour: string;
+  hairColour: string;
+  wand: Wand;
+  patronus: string;
+  hogwartsStudent: boolean;
+  hogwartsStaff: boolean;
+  actor: string;
+  alternate_actors: string[];
+  alive: boolean;
   image: string;
 }
 
