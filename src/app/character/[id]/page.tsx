1be27d77fa5dea487f61428269ab45f57a4ddc7d
Filipe Miranda <email@filipemiranda.dev>
2025-02-25 15:32:53 -0300

    feat: :sparkles: add a back to characters button

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,10 +1,23 @@
 import { CharacterInfo } from "@/components/character-info";
 import { HydrationBoundary } from "@tanstack/react-query";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa6";
 
 export default async function Character() {
   return (
-    <HydrationBoundary>
-      <CharacterInfo />
-    </HydrationBoundary>
+    <>
+      <div className="w-full">
+        <Link
+          href="/characters"
+          className="flex items-center gap-2 hover:underline"
+        >
+          <FaArrowLeft />
+          Back to Characters
+        </Link>
+      </div>
+      <HydrationBoundary>
+        <CharacterInfo />
+      </HydrationBoundary>
+    </>
   );
 }
